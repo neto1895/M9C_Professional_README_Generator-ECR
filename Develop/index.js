29b8bbf9b063a86557f3903984b8eadbcd2b3e20
neto1895 <cruz.rosales.ernesto@gmail.com>
2023-05-14 20:49:24 -0600

    Used generateMarkdown for the activity

--- Develop/index.js
+++ Develop/index.js
@@ -5,10 +5,10 @@ const { default: Choice } = require('inquirer/lib/objects/choice');
 // TODO: Create an array of questions for user input
 const licenses = [
     { name: '- MIT',        value: "MIT" },
-    { name: '- APACHE 2.0', value: "APACHE 2.0" },
-    { name: '- GPL 3.0',    value: "GPL 3.0" },
-    { name: '- GNU V3.0',   value: "GNU V3.0" },
-    { name: '- BSD 3',      value: "BSD 3" },
+    { name: '- APACHE 2.0', value: "APACHE%202.0" },
+    { name: '- GPL 3.0',    value: "GPL%203.0" },
+    { name: '- GNU V3.0',   value: "GNU%20V3.0" },
+    { name: '- BSD 3',      value: "BSD%203" },
     { name: '- None',       value: "None" },      
 ];
 
@@ -25,77 +25,17 @@ const questions = () =>{
         { type: 'input',name: 'contribution',message: 'What does the user need to know about contributing to the repo?'  },
 ])}
 
-const generateREADME = ({username, email, project, description, license, installation, test, usage, contribution}) =>
-`
-![Web Developer](https://img.shields.io/badge/bootcamp-Web%20Developer-red)
-# ${project}
 
-![Web Developer](https://img.shields.io/badge/license-${license}-blue)
-## Description
-
-${description}
-
-## Table of Contents
-- [ Instalation ](#instalation)
-- [ Usage ](#usage)
-- [ License](#license)
-- [ Contributing](#Contributing)
-- [ Test ](#test)
-- [ Questions ](#questions)
-
-<a name="instalation"></a>
-
-## Instalation
-
-To install necessary dependencies, run the following command:
-
-> ${installation}
-
-<a name="usage"></a>
-
-## Usage
-
-${usage}
-
-<a name="license"></a>
-
-## License
-
-This project is licensed under the -license- license.
-
-
-
-<a name="Contributing"></a>
-
-## Contributing
-
-${contribution}
-
-<a name="test"></a>
-
-## Test
-
-To run test, run the following command:
-
-> ${test}
-
-<a name="questions"></a>
-
-## Questions
-
-If you have any questions about the repo, open an issue or contact me directly at [${email}](mailto:${email}). You can find more of my work at [${username}](https://github.com/${username})
-
-![Web Developer](https://img.shields.io/badge/bootcamp-Web%20Developer-red)
-`
 // TODO: Create a function to write README file
 function writeToFile(answers) {
-    console.log("test: "+answers.email+" and ");
+
     const generateMarkdown = require("./utils/generateMarkdown");
     generateMarkdown.renderLicenseBadge(3);
-    console.log(generateMarkdown.generateMarkdown(4));
 
 
-    const readmeContent = generateREADME(answers)
+
+
+    const readmeContent = generateMarkdown.generateMarkdown(answers)
 
     fs.writeFile("README.md",readmeContent,(err) =>
     err ? console.log(err): console.log("Successfully created README.md!")
@@ -106,10 +46,10 @@ function writeToFile(answers) {
 function init() {
 questions()
 
-    .then((answers) => {
-        writeToFile(answers);
-    }
-    );
+.then((answers) => {
+    writeToFile(answers);
+}
+);
 }
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,11 +13,68 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data}
-
-`;
-}
+  const generateMarkdown = ({username, email, project, description, license, installation, test, usage, contribution}) =>
+  `
+  ![Web Developer](https://img.shields.io/badge/bootcamp-Web%20Developer-red)
+  # ${project}
+  
+  ![Web Developer](https://img.shields.io/badge/license-${license}-blue)
+  ## Description
+  
+  ${description}
+  
+  ## Table of Contents
+  - [ Instalation ](#instalation)
+  - [ Usage ](#usage)
+  - [ License](#license)
+  - [ Contributing](#Contributing)
+  - [ Test ](#test)
+  - [ Questions ](#questions)
+  
+  <a name="instalation"></a>
+  
+  ## Instalation
+  
+  To install necessary dependencies, run the following command:
+  
+  > ${installation}
+  
+  <a name="usage"></a>
+  
+  ## Usage
+  
+  ${usage}
+  
+  <a name="license"></a>
+  
+  ## License
+  
+  This project is licensed under the -license- license.
+  
+  
+  
+  <a name="Contributing"></a>
+  
+  ## Contributing
+  
+  ${contribution}
+  
+  <a name="test"></a>
+  
+  ## Test
+  
+  To run test, run the following command:
+  
+  > ${test}
+  
+  <a name="questions"></a>
+  
+  ## Questions
+  
+  If you have any questions about the repo, open an issue or contact me directly at [${email}](mailto:${email}). You can find more of my work at [${username}](https://github.com/${username})
+  
+  ![Web Developer](https://img.shields.io/badge/bootcamp-Web%20Developer-red)
+  `;
 
 module.exports = {
   renderLicenseBadge,
